0764bdc826a335236ffef909ccb54f67d6a23815
Stay4U <rornfleodud@gmail.com>
2022-03-15 16:49:58 +0900

    Update HeicToJpgWithSwing.java

--- HeicToJpgWithSwing.java
+++ HeicToJpgWithSwing.java
@@ -2,6 +2,7 @@ import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
+import java.util.LinkedList;
 
 import javax.swing.DefaultListModel;
 import javax.swing.JButton;
@@ -10,6 +11,7 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JList;
 import javax.swing.JScrollPane;
+import javax.swing.ListModel;
 import javax.swing.filechooser.FileSystemView;
 
 public class HeicToJpg {
@@ -17,44 +19,48 @@ public class HeicToJpg {
 	private JLabel srcFolderLbl;
 	private JButton folderOpenBtn;
 	private JButton fileConvertBtn;
-	private DefaultListModel<String> listModel;
-	private JList<DefaultListModel<String>> srcFileListLbl;
+	private JList<String> srcFileList;
+	private DefaultListModel<String> srcFileListModel;
 	private JScrollPane scrolled;
 
 	private void prepareGUI() {
 		mf = new JFrame("Heic To Jpg");
 		mf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		mf.setLocation(10, 10);
-		mf.setSize(600, 600);
+		mf.setSize(550, 850);
 		mf.setLayout(null);
 		
+		/*
 		srcFolderLbl = new JLabel("원본폴더");
 		srcFolderLbl.setLocation(10, 10);
 		srcFolderLbl.setSize(100, 50);
 		srcFolderLbl.setBackground(Color.darkGray);
 		srcFolderLbl.setOpaque(true);
 		mf.add(srcFolderLbl);
+		*/
+		
+		ButtonClickListener buttonClickListener = new ButtonClickListener();
 		
 		folderOpenBtn = new JButton("폴더열기");
 		folderOpenBtn.setActionCommand("folderOpen");
-		folderOpenBtn.setLocation(120, 10);
+		folderOpenBtn.setLocation(10, 15);
 		folderOpenBtn.setSize(100, 50);
-		folderOpenBtn.addActionListener(new ButtonClickListener());
+		folderOpenBtn.addActionListener(buttonClickListener);
 		mf.add(folderOpenBtn);
 
 		fileConvertBtn = new JButton("파일변환");
 		fileConvertBtn.setActionCommand("fileConvert");
-		fileConvertBtn.setLocation(330, 10);
+		fileConvertBtn.setLocation(120, 15);
 		fileConvertBtn.setSize(100, 50);
-		fileConvertBtn.addActionListener(new ButtonClickListener());
+		fileConvertBtn.addActionListener(buttonClickListener);
 		mf.add(fileConvertBtn);
 		
-		listModel = new DefaultListModel<String>();
-		srcFileListLbl = new JList(listModel);
-		listModel.addElement("123");
-		scrolled = new JScrollPane(srcFileListLbl);
-		scrolled.setLocation(10, 100);
-		scrolled.setSize(50, 50);
+		srcFileList = new JList<String>(new DefaultListModel<String>());
+		srcFileListModel = (DefaultListModel<String>)srcFileList.getModel();
+		
+		scrolled = new JScrollPane(srcFileList);
+		scrolled.setLocation(10, 80);
+		scrolled.setSize(513, 713);
 		mf.add(scrolled);
 		
 		mf.setVisible(true);
@@ -65,10 +71,29 @@ public class HeicToJpg {
 			String command = e.getActionCommand();
 
 			if (command.equals("folderOpen")) {
-				//String path = jFileChooserUtil();
+
 				//statusLabel.setText(path);
+				String path = jFileChooserUtil();
+				if( "".equals(path) ) {
+					return;
+				}
+				
+				if(srcFileListModel.size() > 0) {
+					srcFileListModel.removeAllElements();
+				}
+				
+				LinkedList<String> list = new LinkedList<String>();
+				
+				RecursiveGetFileList(path, list);
+				
+				for (int i = 0; i < list.size(); i++) {
+					srcFileListModel.addElement(list.get(i));
+				}
+				
 			} else if (command.equals("fileConvert")) {
-				//statusLabel.setText("Submit Button clicked.");
+				if(srcFileListModel.size() > 0) {
+					srcFileListModel.removeAllElements();
+				}
 			} else {
 				//statusLabel.setText("Cancel Button clicked.");
 			}
@@ -93,6 +118,29 @@ public class HeicToJpg {
         }
         return folderPath;        
     }
+	
+	public static LinkedList<String> RecursiveGetFileList(String dirPath, LinkedList<String> rtnFileList) {
+		
+		File file = new File(dirPath);
+		File[] fileList = file.listFiles();
+		
+		for (int i = 0; i < fileList.length; i++) {
+			File fileUnit = fileList[i];
+			String path = fileUnit.getPath();
+			
+			if( fileUnit.isFile() ) {
+				String fileName = fileUnit.getName();
+				if("".equals(fileName)) continue;
+				String ext = fileName.substring(fileName.lastIndexOf(".")+1).toLowerCase();
+				if( "heic".equals(ext) ) {
+					rtnFileList.add(fileUnit.getPath());
+				}
+			}else if( fileUnit.isDirectory() ) {
+				RecursiveGetFileList(path, rtnFileList);
+			}
+		}
+		return rtnFileList;
+	}
 	
 	public static void main(String[] args) {
 		new HeicToJpg().prepareGUI();
